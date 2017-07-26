4488219c6eb7f7a24489454d338d5962f367ed9e
martincaddy <martincaddy@gmail.com>
2017-07-26 10:31:59 +0100

    finish and function

--- snitch-sniffer.py
+++ snitch-sniffer.py
@@ -1,11 +1,6 @@
 #Some dummy code to get going
 from time import sleep
-def hp():
-	print('The Snitch Sniffer')
-	sleep(1)
-	print('Harry Potter Rules')
-	sleep(1)
-	print('Voldemort is bad')
+
 
 for i in range 5:
 	hp
